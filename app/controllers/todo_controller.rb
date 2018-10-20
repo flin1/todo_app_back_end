25a37f74d923dfd2f4c4d2b78c9abfcb6d4969e9
flin <flin@cps.edu>
2018-10-20 18:11:14 +0000

    did description

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,7 +2,8 @@ class TodoController < ApplicationController
   def index
   end
   def show
-    @todo_description = "Make the curriculum"
-     @todo_pomodoro_estimate = 4
+    @todo_description = params[:id]
+    @todo_pomodoro_estimate = 4
+     
   end
 end
