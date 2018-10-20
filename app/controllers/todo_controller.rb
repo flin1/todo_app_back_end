7356c03118343c83d852bcf1d41bc09d8bdc47fc
flin <flin@cps.edu>
2018-10-20 19:50:38 +0000

    finished linking

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,8 +2,23 @@ class TodoController < ApplicationController
   def index
   end
   def show
-    @todo_description = params[:id]
+     todo_id =  params[:id]
+     if todo_id == '1'
+    @todo_description = "do homework"
     @todo_pomodoro_estimate = 4
-     
+   
+   elsif todo_id == '2'
+    @todo_description = "clean the bathroom"
+    @todo_pomodoro_estimate = 1
+    
+    elsif todo_id == '3'
+    @todo_description = "eat food"
+    @todo_pomodoro_estimate = 2
+    
+    elsif todo_id == '4'
+    @todo_description = "knit"
+    @todo_pomodoro_estimate = "as many pomodoros as I want"
+    
+     end
   end
 end
